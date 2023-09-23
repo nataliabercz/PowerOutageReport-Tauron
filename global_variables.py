acf982221a9fd7c3e1897696d75de95a15a18685
nataliabercz <nataliabercz@gmail.com>
2023-09-23 09:32:31 +0200

    Added file with global variables

---
+++ global_variables.py
@@ -0,0 +1,8 @@
+URL = 'https://www.tauron-dystrybucja.pl/waapi/{}'
+JSON_CONFIGURATION_FILE = 'configuration.json'
+IN_FILE = 'in `{}` file.'.format(JSON_CONFIGURATION_FILE)
+WRONG_FILE = 'The `{}` file is not correct.'.format(JSON_CONFIGURATION_FILE)
+MISSING_OR_EMPTY_PARAMETER = 'The parameter: `{}` is missing or empty ' + IN_FILE
+WRONG_STREET_NAME_OR_CITY = 'The street name: `{}` or city `{}` is not correct ' + IN_FILE
+WRONG_PARAMETER_TYPE = 'The parameter: `{}` should have `{}` type ' + IN_FILE
+WRONG_REQUEST = 'Wrong request! Status code: {}.'
