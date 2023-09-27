149de0aefe3ebece55bfd7850587d0abc7b8eb2d
nataliabercz <nataliabercz@gmail.com>
2023-09-27 17:45:13 +0200

    Added global variables and tests data files

--- global_variables.py
+++ global_variables.py
@@ -1,8 +1,19 @@
+import os
+
 URL = 'https://www.tauron-dystrybucja.pl/waapi/{}'
+
 JSON_CONFIGURATION_FILE = 'configuration.json'
+APP_PATH = '{}/data/{}'.format(os.path.dirname(os.path.realpath(__file__)), JSON_CONFIGURATION_FILE)
 IN_FILE = 'in `{}` file.'.format(JSON_CONFIGURATION_FILE)
+FILE_DOESNT_EXIST = "The `{}` file doesn't exist".format(JSON_CONFIGURATION_FILE)
 WRONG_FILE = 'The `{}` file is not correct.'.format(JSON_CONFIGURATION_FILE)
 MISSING_OR_EMPTY_PARAMETER = 'The parameter: `{}` is missing or empty ' + IN_FILE
 WRONG_STREET_NAME_OR_CITY = 'The street name: `{}` or city `{}` is not correct ' + IN_FILE
 WRONG_PARAMETER_TYPE = 'The parameter: `{}` should have `{}` type ' + IN_FILE
+
 WRONG_REQUEST = 'Wrong request! Status code: {}.'
+CONNECTION_ERROR = 'Connection ERROR.'
+CONNECTION_ERROR_RETRYING = CONNECTION_ERROR + 'Retrying in {}s... {}'
+
+EMAIL_TITLE = '[TAURON] Planned power outage - {}'
+EMAIL_WAS_SENT = 'The email was sent to: {}'
--- tests/test_power_outage_report_data.py
+++ tests/test_power_outage_report_data.py
@@ -39,9 +39,39 @@ correct_configuration_multiple_addresses = {
 test_response = '{"AddressPoint":null,"OutageItems":[{"OutageId":"id_1","StartDate":"s_date","EndDate":"e_date",' \
                 '"Message":"msg","IsActive":true},{"OutageId":"id_2","StartDate":"s_date","EndDate":"e_date",' \
                 '"Message":"msg","IsActive":false}]}'
-test_response_adjusted = '{"AddressPoint":"","OutageItems":[{"OutageId":"id_1","StartDate":"s_date",' \
-                         '"EndDate":"e_date","Message":"msg","IsActive":True},{"OutageId":"id_2",' \
-                         '"StartDate":"s_date","EndDate":"e_date","Message":"msg","IsActive":False}]}'
+test_response_replaced = '{"AddressPoint":"","OutageItems":[{"OutageId":"id_1","StartDate":"s_date",' \
+                         '"EndDate":"e_date","Message":"msg","IsActive":True},{"OutageId":"id_2","StartDate":"s_date",' \
+                         '"EndDate":"e_date","Message":"msg","IsActive":False}]}'
+test_response_adjusted = {'AddressPoint': '', 'OutageItems': [{'EndDate': 'e_date', 'IsActive': True, 'Message': 'msg',
+                                                               'OutageId': 'id_1', 'StartDate': 's_date'},
+                                                              {'EndDate': 'e_date', 'IsActive': False, 'Message': 'msg',
+                                                               'OutageId': 'id_2', 'StartDate': 's_date'}]}
 
-email_template = 'From: {}\nTo: {}\nSubject: [TAURON] Planned power outage\n\nAREA: {}\nSTART: {}\nEND: {}\n' \
+outage_items = [{'OutageId': 'id_1',
+                 'StartDate': 's_date_1',
+                 'EndDate': 'e_date_1',
+                 'Message': 'StreetName'},
+                {'OutageId': 'id_2',
+                 'StartDate': 's_date_2',
+                 'EndDate': 'e_date_2',
+                 'Message': 'OtherStreet'}]
+
+email_template = 'From: {}\nTo: {}\nSubject: [TAURON] Planned power outage - {}\n\nAREA: {}\nSTART: {}\nEND: {}\n' \
                  'DURATION: {} - {}'
+
+sent_emails_folder = '[Gmail]/Sent Mail'
+sent_email_message_bytes = b'Return-Path: <sender@mail.com>\r\n' \
+                           b'Received: from LAPTOP-XYZ.lan ([0.0.0.0])\r\n' \
+                           b'by smtp.gmail.com with ESMTPSA id l25-20020a2m3024657ljg.111.2023.09.27.06.43.59\r\n' \
+                           b'for <receiver@mail.com>\r\n' \
+                           b'(version=TLS1_3 cipher=TLS_AES_256_GCM_SHA384 bits=256/256);\r\n' \
+                           b'Wed, 27 Sep 2023 02:43:59 -0700 (PDT)\r\n' \
+                           b'Message-ID:<6513f95f.2e0a0220.53138.8d3c@mx.google.com>\r\n' \
+                           b'Date: Wed, 27 Sep 2023 06:43:59 -0700 (PDT)\r\n' \
+                           b'From: sender@mail.com\r\n' \
+                           b'To: receiver@mail.com\r\n' \
+                           b'Subject: [TAURON] Planned power outage - StreetName\r\n\r\n' \
+                           b'AREA: ... StreetName ...\r\n' \
+                           b'START: 27/09/2023\r\n' \
+                           b'END: 27/09/2023\r\n' \
+                           b'DURATION: 07:00:00 - 16:00:00\r\n'
